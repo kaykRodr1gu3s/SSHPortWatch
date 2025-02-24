c13cb0c1c847ca9310d17e49dcee162b5789630a
kaykRodr1gu3s <110197812+kaykRodr1gu3s@users.noreply.github.com>
2025-02-24 13:18:58 -0300

    excel file creator
    New feature: function that create an excel file

--- main.py
+++ main.py
@@ -31,8 +31,30 @@ class Server:
             for num in range(3, len(datas) + 1, 3):
                 df_data.append(datas[count:num])
                 count = num
+     
+    @property
+    def clean_data(self):
+        with open("nmap.txt", mode="r") as file:
+            lines = file.read()
+            datas = lines.split("(conn-refused)")[1]
+            datas = datas.split("Nmap done")[0].split()
+            count = 0
+            clean_datas = [datas[:3]]
+            del datas[:3]
+            for num in range(3, len(datas) + 1, 3):
+                clean_datas.append(datas[count:num])
+                count = num
+
+        def excel_files(df_data):
+
+            df = pd.DataFrame(df_data)
+            df.to_excel("server_listen_ports.xlsx")
+
+        excel_files(clean_datas)
 
     def main(self):
+        
         self.schedule_task
         self.download_file
         self.clean_data
+
